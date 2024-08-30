9035b086dcadd3c8ceaec6311561e9b8a33ef628
Andrei Lomoloy <andreilouielomoloy@gmail.com>
2024-08-30 15:38:47 +0800

    Update TableAndChairs.java

--- Chapter 1/TableAndChairs.java
+++ Chapter 1/TableAndChairs.java
@@ -1,35 +1,11 @@
-public class TableAndChairs {
-
-    public static void main(String[] args) {
-     
-        TableAndChairs tableAndChairs = new TableAndChairs();
-
-       
-        tableAndChairs.printPattern();
-    }
-
-    
-    public void printPattern() {
-       
-        System.out.println("X");
-        System.out.println("XXXXXXXXXX");
-        System.out.println("X");
-
-        
-        for (int i = 0; i < 4; i++) {
-            System.out.print("XXXXXX ");
-            for (int j = 0; j < i; j++) {
-                System.out.print("X");
-            }
-            System.out.println(" XXXXX");
-        }
-
-        
-        System.out.println("X X");
-        System.out.println("X");
-        System.out.println("X X X");
-        System.out.println("XX");
-        System.out.println("X");
-        System.out.println("X X X");
-    }
+ class TableAndChairs {
+   public static void main(String[] args)
+   {
+      System.out.println("X                      X");
+      System.out.println("X                      X");
+      System.out.println("X      XXXXXXXXXX      X");
+      System.out.println("XXXXX  X        X  XXXXX");
+      System.out.println("X   X  X        X  X   X");
+      System.out.println("X   X  X        X  X   X");
+   }
 }
