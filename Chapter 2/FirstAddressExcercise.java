f05a2e10942df2f8940c502f61cbc814c72ea2d7
Andrei Lomoloy <andreilouielomoloy@gmail.com>
2024-09-16 19:24:02 +0800

    Create FirstAddressExcercise.java

---
+++ Chapter 2/FirstAddressExcercise.java
@@ -0,0 +1,14 @@
+public class FirstAddressExcercise {
+
+    public static void main(String[] args) {
+        System.out.println("First Java application");
+        displayAddress();
+    }
+
+    public static void displayAddress() {
+        System.out.println("Samsung Electronics Co., Ltd.");
+        System.out.println("129 Samsung-ro, Yeongtong-gu");
+        System.out.println("Suwon-si, Gyeonggi-do 16677");
+        System.out.println("Company: Samsung Electronics Co., Ltd.");
+    }
+}
